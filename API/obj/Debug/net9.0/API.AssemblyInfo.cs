b67caf029212859b93367a8426a96d6a22e11f9f
sibeliouss <sibelmozturk@hotmail.com>
2025-02-01 13:17:49 +0300

    Error Handling

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f29e177da2a18b463574bcd53b1099f4b995e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d393dd1c7705847f46de478ae68814be3586b3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
